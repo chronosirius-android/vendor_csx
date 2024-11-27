df09ca6435b26e1b1710b6e8a4bd09c16e49845d
chronosirius <chronosirius@gmail.com>
2024-11-27 10:52:40 -0800

    Added nethunter makefiles for later
    Change-Id: I328adfd11b81c621248dd963e4415bfa14e5d660

---
+++ nethunter.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+	vendor/csx/proprietary/odm/firmware/mt7662.bin:$(TARGET_COPY_OUT_ODM)/firmware/mt7662.bin \
+	vendor/csx/proprietary/odm/firmware/mt7662_rom_patch.bin:$(TARGET_COPY_OUT_ODM)/firmware/mt7662_rom_patch.bin \
+	vendor/csx/proprietary/odm/firmware/mt7662u.bin:$(TARGET_COPY_OUT_ODM)/firmware/mt7662u.bin \
+	vendor/csx/proprietary/odm/firmware/mt7662u_rom_patch.bin:$(TARGET_COPY_OUT_ODM)/firmware/mt7662u_rom_patch.bin \
+
