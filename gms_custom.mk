54e41bbb2b62b95d119b469b50de5d98cd3e6b31
chronosirius <chronosirius@gmail.com>
2026-01-25 02:33:11 +0000

    updated gms_custom to match newest full

--- gms_custom.mk
+++ gms_custom.mk
@@ -17,106 +17,127 @@
 #
 
 # Quick Tap
+TARGET_SUPPORTS_QUICK_TAP ?= true
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
 PRODUCT_PACKAGES += \
     quick_tap
+endif
 
 # product/app
 PRODUCT_PACKAGES += \
-    CalculatorGooglePrebuilt_85005407 \
-    CalendarGooglePrebuilt \
+    CalculatorGooglePrebuilt_85006267 \
     Chrome \
     Chrome-Stub \
-    GeminiPrebuilt \
+    DevicePolicyPrebuilt-v10334460 \
     GoogleContacts \
     LatinIMEGooglePrebuilt \
     LocationHistoryPrebuilt \
     MarkupGoogle_v2 \
     NgaResources \
+    NowPlayingPrebuilt \
     Photos \
-    PixelThemesStub \
-    PixelThemesStub2022_and_newer \
-    PlayAutoInstallConfig \
-    PrebuiltDeskClockGoogle_76003530 \
-    SoundPickerPrebuilt \
+    PixelThemesStub2025_and_newer \
+    SoundAmplifierPrebuilt_v4.8.758106410 \
+    SoundPickerPrebuilt_33000062 \
     TrichromeLibrary \
     TrichromeLibrary-Stub \
+    WallpaperAIPrebuilt_10000372 \
     WebViewGoogle \
     WebViewGoogle-Stub \
-    arcore-1.42 \
-    talkback
 
-ifneq ($(filter felix comet, $(LINEAGE_BUILD)),)
+TARGET_INCLUDE_LIVE_WALLPAPERS ?= true
+ifeq ($(TARGET_INCLUDE_LIVE_WALLPAPERS),true)
 PRODUCT_PACKAGES += \
-    PixelWallpapers2023Foldable
+    PixelWallpapers2025
+endif
+
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet tegu frankel blazer mustang, $(LINEAGE_BUILD)),)
+PRODUCT_PACKAGES += \
+    Tycho
+endif
+
+TARGET_INCLUDE_STOCK_ARCORE ?= true
+ifeq ($(TARGET_INCLUDE_STOCK_ARCORE),true)
+PRODUCT_PACKAGES += \
+    arcore-1.48
 endif
 
 # product/priv-app
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet tegu frankel blazer mustang, $(LINEAGE_BUILD)),)
 PRODUCT_PACKAGES += \
-    AICorePrebuilt-aicore_20240509.01_RC02 \
-    AdaptiveVPNPrebuilt-10307 \
-    AmbientStreaming \
-    AndroidAutoStubPrebuilt \
-    BetterBugStub \
-    CarrierLocation \
-    CarrierMetrics \
-    CbrsNetworkMonitor \
-    ConfigUpdater \
-    CreativeAssistant \
-    DeviceIntelligenceNetworkPrebuilt-U.21_playstore_astrea_20240222.00_RC01 \
-    DevicePersonalizationPrebuiltPixel2024-U.21_P24_aiai_20240327.00_RC01 \
-    FilesPrebuilt \
-    GCS \
-    GoogleDialer \
-    GoogleOneTimeInitializer \
-    GoogleRestorePrebuilt-v509024 \
-    MaestroPrebuilt \
-    OdadPrebuilt \
-    PartnerSetupPrebuilt \
-    Phonesky \
-    PixelAIPrebuilt \
-    PixelLiveWallpaperPrebuilt \
-    PrebuiltBugle \
-    RecorderPrebuilt \
-    SafetyHubPrebuilt \
-    ScribePrebuilt \
-    SearchSelectorPrebuilt \
-    SettingsIntelligenceGooglePrebuilt \
-    SetupWizardPrebuilt \
-    TurboPrebuilt \
-    UvExposureReporter \
-    Velvet \
-    WallpaperEffect \
-    WellbeingPrebuilt
+    SCONE
+endif
 
-ifneq ($(filter flame coral redfin oriole raven panther cheetah lynx felix shiba husky akita tokay caiman komodo comet, $(LINEAGE_BUILD)),)
+ifneq ($(filter flame coral redfin oriole raven panther cheetah lynx felix shiba husky akita tokay caiman komodo tegu frankel blazer mustang, $(LINEAGE_BUILD)),)
 PRODUCT_PACKAGES += \
+    DreamlinerDreamsPrebuilt \
     DreamlinerPrebuilt \
     DreamlinerUpdater
 endif
 
-ifneq ($(filter caiman komodo, $(LINEAGE_BUILD)),)
+ifneq ($(filter husky akita tokay caiman komodo frankel blazer mustang, $(LINEAGE_BUILD)),)
 PRODUCT_PACKAGES += \
-    HealthIntelligencePrebuilt-1762
+    HealthIntelligencePrebuilt
 endif
 
-ifneq ($(filter shiba husky akita, $(LINEAGE_BUILD)),)
+TARGET_INCLUDE_LIVE_WALLPAPERS ?= true
+ifeq ($(TARGET_INCLUDE_LIVE_WALLPAPERS),true)
 PRODUCT_PACKAGES += \
-    HealthIntelligenceStubPrebuilt
+    PixelLiveWallpaperPrebuilt-26000013
 endif
 
-ifneq ($(filter blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet, $(LINEAGE_BUILD)),)
+TARGET_SUPPORTS_GOOGLE_RECORDER ?= true
+ifeq ($(TARGET_SUPPORTS_GOOGLE_RECORDER),true)
 PRODUCT_PACKAGES += \
-    SCONE-v31427
+    RecorderPrebuilt_725581069
 endif
+
+PRODUCT_PACKAGES += \
+    AICorePrebuilt-aicore_20250130.00_RC01 \
+    AmbientStreaming \
+    AndroidAutoStubPrebuilt \
+    BetterBugStub \
+    CarrierLocation \
+    CbrsNetworkMonitor \
+    ConfigUpdater \
+    DeviceIntelligenceNetworkPrebuilt-astrea_20240329.00_RC02 \
+    DevicePersonalizationPrebuiltPixel2025-playstore_aiai_20250306.00_RC10 \
+    FilesPrebuilt \
+    GeminiShell_227 \
+    GoogleDialer \
+    GoogleOneTimeInitializer \
+    GoogleRestorePrebuilt-v793553 \
+    KidsSupervisionStub \
+    MaestroPrebuilt \
+    OdadPrebuilt \
+    PartnerSetupPrebuilt \
+    Phonesky \
+    PrebuiltBugle \
+    PrebuiltDeskClockGoogle_76007351 \
+    PrebuiltPixelCoreServices \
+    RelationshipsPrebuilt-301 \
+    SafetyHubPrebuilt \
+    ScribePrebuilt_v8.2.717788270 \
+    SearchSelectorPrebuilt \
+    SettingsIntelligenceGooglePrebuilt \
+    SetupWizardPrebuilt_bd3a \
+    SubzeroPrebuilt-stub-v2 \
+    TurboPrebuilt \
+    Velvet \
+    WallpaperEffect \
+    WellbeingPrebuilt
 
 # system/app
 PRODUCT_PACKAGES += \
+#    CaptivePortalLoginGoogle \
     GoogleExtShared \
     GooglePrintRecommendationService
 
 # system/priv-app
 PRODUCT_PACKAGES += \
-    DocumentsUIGoogle
+    DocumentsUIGoogle \
+    GooglePackageInstaller \
+    TagGoogle
 
 # system_ext/app
 PRODUCT_PACKAGES += \
@@ -124,32 +145,42 @@ PRODUCT_PACKAGES += \
     Flipendo
 
 # system_ext/priv-app
+ifneq ($(filter sailfish marlin walleye taimen blueline crosshatch sargo bonito flame coral sunfish bramble redfin barbet oriole raven bluejay panther cheetah lynx tangorpro felix shiba husky akita tokay caiman komodo comet tegu frankel blazer mustang, $(LINEAGE_BUILD)),)
 PRODUCT_PACKAGES += \
+    DeviceConnectivityServicePrebuilt_25.03.00
+endif
+
+TARGET_INCLUDE_LIVE_WALLPAPERS ?= true
+ifeq ($(TARGET_INCLUDE_LIVE_WALLPAPERS),true)
+PRODUCT_PACKAGES += \
+    MagicPortraitWallpapers
+endif
+
+PRODUCT_PACKAGES += \
+    AvatarPickerGoogle \
+    GoogleFeedback \
     GoogleServicesFramework \
+    MoseyApp \
     NexusLauncherRelease \
-    SetupWizardPixelPrebuilt \
+    SetupWizardPixelPrebuilt_bd3a \
     StorageManagerGoogle \
     TurboAdapter \
     WallpaperPickerGoogleRelease
 
-ifneq ($(filter tokay caiman komodo, $(LINEAGE_BUILD)),)
-PRODUCT_PACKAGES += \
-    VendorSatelliteService
-endif
-
 # PrebuiltGmsCore
 PRODUCT_PACKAGES += \
-    PrebuiltGmsCoreSc \
-    PrebuiltGmsCoreSc_AdsDynamite.uncompressed \
-    PrebuiltGmsCoreSc_CronetDynamite.uncompressed \
-    PrebuiltGmsCoreSc_DynamiteLoader.uncompressed \
-    PrebuiltGmsCoreSc_DynamiteModulesA \
-    PrebuiltGmsCoreSc_DynamiteModulesC \
-    PrebuiltGmsCoreSc_GoogleCertificates \
-    PrebuiltGmsCoreSc_MapsDynamite \
-    PrebuiltGmsCoreSc_MeasurementDynamite \
+    PrebuiltGmsCoreVic \
+    PrebuiltGmsCoreVic_AdsDynamite \
+    PrebuiltGmsCoreVic_CronetDynamite \
+    PrebuiltGmsCoreVic_DynamiteLoader \
+    PrebuiltGmsCoreVic_DynamiteModulesA \
+    PrebuiltGmsCoreVic_DynamiteModulesC \
+    PrebuiltGmsCoreVic_GoogleCertificates \
+    PrebuiltGmsCoreVic_MapsDynamite \
+    PrebuiltGmsCoreVic_MeasurementDynamite \
     AndroidPlatformServices \
     MlkitBarcodeUIPrebuilt \
+    TfliteDynamitePrebuilt \
     VisionBarcodePrebuilt
 
 # Safety Information
